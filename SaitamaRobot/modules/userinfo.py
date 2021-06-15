f62e6bdc43e1bab8f5cab9291d385675fe21ce3c
IzumiCypherX <85168398+IzumiCypherX@users.noreply.github.com>
2021-06-15 12:41:19 +0530

    Update userinfo.py

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -107,7 +107,7 @@ def hpmanager(user):
 
 def make_bar(per):
     done = min(round(per / 10), 10)
-    return "■" * done + "□" * (10 - done)
+    return "💠" * done + "🔸️" * (10 - done)
 
 
 @run_async
