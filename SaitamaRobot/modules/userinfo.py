092025cdf64a17035f3aa81304810bc4ae3a0f0a
IzumiCypherX <85168398+IzumiCypherX@users.noreply.github.com>
2021-06-27 23:00:16 +0530

    Update userinfo.py

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -514,7 +514,7 @@ Examples:
  • `/info`*:* get information about a user. 
  
 *What is that health thingy?*
- Come and see [HP System explained](https://t.me/YumekoJabamiUpdates/6)
+ Come and see [HP System explained](https://t.me/EmiliaBotUpdates/5) 
 """
 
 SET_BIO_HANDLER = DisableAbleCommandHandler("setbio", set_about_bio)
