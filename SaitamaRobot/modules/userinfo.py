c1b9c1c35250a2be0ec3db75e152a771f6e8b681
IzumiCypherX <85168398+IzumiCypherX@users.noreply.github.com>
2021-06-27 22:54:39 +0530

    Update userinfo.py

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -290,7 +290,7 @@ def info(update: Update, context: CallbackContext):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' [<a href="https://t.me/EmiliaAnimeBotSupport/1312">?</a>]'.format(
+        text += ' [<a href="https://t.me/EmiliaBotUpdates/4">?</a>]'.format(
             bot.username)
 
     try:
