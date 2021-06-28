d4384e0aa4848fd5bfd4bbfb96f80cf3e7d41cd6
IzumiCypherX <85168398+IzumiCypherX@users.noreply.github.com>
2021-06-28 15:59:56 +0530

    Update songs.py

--- SaitamaRobot/modules/songs.py
+++ SaitamaRobot/modules/songs.py
@@ -58,7 +58,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎙 **Title**: [{title[:35]}]({link})\n🎬 **Source**: YouTube\n⏱️ **Duration**: `{duration}`\n👁‍🗨 **Views**: `{views}`\n📤 **By**: @yumekojabamisupport '
+        rep = f'🎙 **Title**: [{title[:35]}]({link})\n🎬 **Source**: YouTube\n⏱️ **Duration**: `{duration}`\n👁‍🗨 **Views**: `{views}`\n📤 **By**: @EmiliaAnimeBot '
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
@@ -66,7 +66,7 @@ def song(client, message):
         message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur)
         m.delete()
     except Exception as e:
-        m.edit('❌ Error report it to @yumekojabamisupport')
+        m.edit('❌ Error report it to @EmiliaAnimeBotSupport')
         print(e)
 
     try:
