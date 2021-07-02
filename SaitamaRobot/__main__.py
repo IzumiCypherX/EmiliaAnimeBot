bb541f5cd4fe522f592634ea941a46cd20d2e6e6
IzumiCypherX <85168398+IzumiCypherX@users.noreply.github.com>
2021-07-03 02:39:34 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -198,9 +198,6 @@ def start(update: Update, context: CallbackContext):
     else:
         update.effective_message.reply_video(
                YUMEKOIMGSTART)
-        update.effective_message.reply_text(
-            "I'm online!\n<b>Up since:</b> <code>{}</code>".format(uptime),
-            parse_mode=ParseMode.HTML)
 
 
 # for test purposes
