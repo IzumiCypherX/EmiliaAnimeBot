35c816e0b23e0cadef62310f9c5862cb0e4e99f2
IzumiCypherX <85168398+IzumiCypherX@users.noreply.github.com>
2021-07-04 16:43:19 +0530

    Update telethonbasics.py

--- SaitamaRobot/pyrogramee/telethonbasics.py
+++ SaitamaRobot/pyrogramee/telethonbasics.py
@@ -2,7 +2,7 @@ import os
 
 from telethon.tl.types import *
 
-from AsunaRobot.pyrogramee.pluginshelper import runcmd
+from SaitamaRobot.pyrogramee.pluginshelper import runcmd
 
 
 async def convert_to_image(event, borg):
