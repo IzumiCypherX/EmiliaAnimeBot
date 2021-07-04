e86baf704233b0faf838e577c511c8484382d1f8
IzumiCypherX <85168398+IzumiCypherX@users.noreply.github.com>
2021-07-04 16:35:33 +0530

    Update errors.py

--- SaitamaRobot/pyrogramee/errors.py
+++ SaitamaRobot/pyrogramee/errors.py
@@ -2,7 +2,7 @@
 import sys
 import traceback
 from functools import wraps
-from AsunaRobot import pbot, SUPPORT_CHAT
+from SaitamaRobot import pgram, SUPPORT_CHAT
 
 def split_limits(text):
     if len(text) < 2048:
