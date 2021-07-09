4d148cc2f8b30333ce017739eb69401c3836b0e8
IzumiCypherX <85168398+IzumiCypherX@users.noreply.github.com>
2021-07-09 08:53:27 +0530

    Update tagall.py

--- SaitamaRobot/modules/tagall.py
+++ SaitamaRobot/modules/tagall.py
@@ -9,8 +9,8 @@
 
 from pyrogram import filters
 
-from AsunaRobot.pyrogramee.pluginshelper import admins_only, get_text
-from AsunaRobot import pbot
+from SaitamaRobot.pyrogramee.pluginshelper import admins_only, get_text
+from AsunaRobot import pgram
 
 
 @pbot.on_message(filters.command("tagall") & ~filters.edited & ~filters.bot)
