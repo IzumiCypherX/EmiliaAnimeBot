4a3c2503c22e0cb02d34399414a169d26d3387a9
Noby007 <87512608+Noby007@users.noreply.github.com>
2021-08-29 12:11:10 +0530

    Update logomaker.py

--- SaitamaRobot/modules/logomaker.py
+++ SaitamaRobot/modules/logomaker.py
@@ -76,11 +76,11 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
     fname2 = "LogoByYone.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @EmiliaAnimeBot")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @nobydayaninfo")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @EmiliaAnimeBotSupport, {e}')
+   await event.reply(f'Error Report @nobydayaninfo, {e}')
 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
