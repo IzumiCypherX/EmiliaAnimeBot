12b6c79d55fb0ec4de89e1571b91c98a84416422
Noby007 <87512608+Noby007@users.noreply.github.com>
2021-08-29 19:08:31 +0530

    Update logomaker.py

--- SaitamaRobot/modules/logomaker.py
+++ SaitamaRobot/modules/logomaker.py
@@ -60,7 +60,7 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./SaitamaRobot/resources/blackbg.jpg')
+    img = Image.open('./SaitamaRobot/resources/PicsArt_08-29-07.03.41.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
