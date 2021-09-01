b48482e23aaebadb150f9628feb8c0a5550247aa
kanekiken44 <81549240+kanekiken44@users.noreply.github.com>
2021-09-01 09:54:43 +0530

    UwU

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -714,6 +714,7 @@ def main():
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(help_handler)
     dispatcher.add_handler(about_callback_handler)
+    dispatcher.add_handler(source_callback_handler)
     dispatcher.add_handler(settings_handler)
     dispatcher.add_handler(help_callback_handler)
     dispatcher.add_handler(settings_callback_handler)
