5d564eb13aa46a793389fb39c89ce487fdbb0721
Cypher Izumi <85168398+IzumiCypherX@users.noreply.github.com>
2021-09-02 09:48:22 +0530

    Update chatbot.py

--- SaitamaRobot/modules/chatbot.py
+++ SaitamaRobot/modules/chatbot.py
@@ -11,7 +11,7 @@ url = "https://acobot-brainshop-ai-v1.p.rapidapi.com/get"
 
 translator = google_translator()
 
-BOT_ID = 1762083152
+BOT_ID = 1975581385
 
 def extract_emojis(s):
     return "".join(c for c in s if c in emoji.UNICODE_EMOJI)
