6d8a7585bf574c0ed2cd40189b201a75b46b97ea
Cypher Izumi <85168398+IzumiCypherX@users.noreply.github.com>
2021-09-05 22:23:47 +0530

    Update chatbotconfig.py

--- SaitamaRobot/modules/chatbotconfig.py
+++ SaitamaRobot/modules/chatbotconfig.py
@@ -6,7 +6,7 @@ from pyrogram import Client
 API_ID = int(os.environ.get("API_ID", "6"))
 API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-ARQ_API_KEY = "XQYJAL-HTSZIK-YALWDS-TJPWMO-ARQ" 
+ARQ_API_KEY = "MTXJXE-ELSWXE-UDRXJC-JCJQXN-ARQ" 
 LANGUAGE = "en"
 ARQ_API_BASE_URL = "https://thearq.tech"
 
