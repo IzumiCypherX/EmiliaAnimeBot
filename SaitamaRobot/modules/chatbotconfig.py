a48884a9dba33e21fec0742da9d9db69b60d9fab
Cypher Izumi <85168398+IzumiCypherX@users.noreply.github.com>
2021-09-05 22:24:48 +0530

    Update chatbotconfig.py

--- SaitamaRobot/modules/chatbotconfig.py
+++ SaitamaRobot/modules/chatbotconfig.py
@@ -3,6 +3,7 @@ import os
 from heroku3 import from_key
 from pyrogram import Client
 
+# My own ARQ API is Given. Don't fuck with it
 API_ID = int(os.environ.get("API_ID", "6"))
 API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
