e9a118586ac1e4185adb9d47c954e44a346c3911
Nautilus <78522210+sudo-nautilus@users.noreply.github.com>
2021-09-08 10:10:23 +0530

    evolution

--- Dockerfile
+++ Dockerfile
@@ -64,12 +64,12 @@ RUN apt update && apt upgrade -y && \
 # Pypi package Repo upgrade
 RUN pip3 install --upgrade pip setuptools
 
-# Copy Python Requirements to /root/SaitamaRobot
-RUN git clone -b Alpha https://github.com/IzumiCypherX/EmiliaAnimeBot /root/SaitamaRobot
-WORKDIR /root/SaitamaRobot
+# Copy Python Requirements to /root/EmiliaAnimeBot
+RUN git clone -b Alpha https://github.com/IzumiCypherX/EmiliaAnimeBot /root/EmiliaAnimeBot
+WORKDIR /root/EmiliaAnimeBot
 
-#Copy config file to /root/SaitamaRobot/SaitamaRobot
-COPY ./SaitamaRobot/sample_config.py ./SaitamaRobot/config.py* /root/SaitamaRobot/SaitamaRobot/
+#Copy config file to /root/EmiliaAnimeBot/EmiliaAnimeBot
+COPY ./EmiliaAnimeBot/sample_config.py ./EmiliaAnimeBot/config.py* /root/EmiliaAnimeBot/EmiliaAnimeBot/
 
 ENV PATH="/home/bot/bin:$PATH"
 
@@ -77,4 +77,4 @@ ENV PATH="/home/bot/bin:$PATH"
 RUN pip3 install -U -r requirements.txt
 
 # Starting Worker
-CMD ["python3","-m","SaitamaRobot"]
+CMD ["python3","-m","EmiliaAnimeBot"]
---
+++ EmiliaAnimeBot/modules/chatbot.py
@@ -0,0 +1,90 @@
+from asyncio import gather, sleep
+
+from pyrogram import filters
+from pyrogram.types import Message
+from aiohttp import ClientSession
+from Python_ARQ import ARQ
+
+from EmiliaAnimeBot import pgram as app
+from EmiliaAnimeBot.pyrogramee.pluginshelper import edit_or_reply as eor
+from EmiliaAnimeBot.pyrogramee.errors import capture_err
+from EmiliaAnimeBot.utils.filter_groups import chatbot_group
+from EmiliaAnimeBot import ARQ_API_KEY
+
+ARQ_API_URL="https://thearq.tech"
+aiohttpsession = ClientSession()
+
+arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
+
+__mod_name__ = "ChatBot"
+__help__ = """
+/chatbot [ENABLE|DISABLE] To Enable Or Disable ChatBot In Your Chat.
+"""
+
+active_chats_bot = []
+
+async def chat_bot_toggle(db, message: Message):
+    status = message.text.split(None, 1)[1].lower()
+    chat_id = message.chat.id
+    if status == "enable":
+        if chat_id not in db:
+            db.append(chat_id)
+            text = "Chatbot Enabled!"
+            return await eor(message, text=text)
+        await eor(message, text="ChatBot Is Already Enabled.")
+    elif status == "disable":
+        if chat_id in db:
+            db.remove(chat_id)
+            return await eor(message, text="Chatbot Disabled!")
+        await eor(message, text="ChatBot Is Already Disabled.")
+    else:
+        await eor(
+            message, text="**Usage:**\n/chatbot [ENABLE|DISABLE]"
+        )
+
+
+
+@app.on_message(filters.command("chatbot") & ~filters.edited)
+@capture_err
+async def chatbot_status(_, message: Message):
+    if len(message.command) != 2:
+        return await eor(
+            message, text="**Usage:**\n/chatbot [ENABLE|DISABLE]"
+        )
+    await chat_bot_toggle(active_chats_bot, message)
+
+
+async def lunaQuery(query: str, user_id: int):
+    luna = await arq.luna(query, user_id)
+    return luna.result
+
+
+async def type_and_send(message: Message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id if message.from_user else 0
+    query = message.text.strip()
+    await message._client.send_chat_action(chat_id, "typing")
+    response, _ = await gather(lunaQuery(query, user_id), sleep(3))
+    await message.reply_text(response)
+    await message._client.send_chat_action(chat_id, "cancel")
+
+
+@app.on_message(
+    filters.text
+    & filters.reply
+    & ~filters.bot
+    & ~filters.via_bot
+    & ~filters.forwarded,
+    group=chatbot_group,
+)
+@capture_err
+async def chatbot_talk(_, message: Message):
+    if message.chat.id not in active_chats_bot:
+        return
+    if not message.reply_to_message:
+        return
+    if not message.reply_to_message.from_user:
+        return
+    if message.reply_to_message.from_user.id != 1975581385:      # This is Emilia's ID, chnage it to yours when deploying
+        return
+    await type_and_send(message)
