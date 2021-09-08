15d225afb9d936452bc40e3094e9a47c594931e5
Nautilus <78522210+sudo-nautilus@users.noreply.github.com>
2021-09-08 10:38:56 +0530

    Emilia Pog

--- EmiliaAnimeBot/modules/fun.py
+++ EmiliaAnimeBot/modules/fun.py
@@ -85,7 +85,7 @@ def slap(update: Update, context: CallbackContext):
     user_id = extract_user(message, args)
 
     if user_id == bot.id:
-        temp = random.choice(fun_strings.SLAP_Emilia_TEMPLATES)
+        temp = random.choice(fun_strings.SLAP_EMILIA_TEMPLATES)
 
         if isinstance(temp, list):
             if temp[2] == "tmute":
