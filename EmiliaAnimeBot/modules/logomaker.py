158735a459d58e98acea4594e1cad66240fb603e
Cypher Izumi <85168398+IzumiCypherX@users.noreply.github.com>
2021-09-09 14:02:46 +0530

    add logo maker(maybe)

--- EmiliaAnimeBot/modules/logomaker.py
+++ EmiliaAnimeBot/modules/logomaker.py
@@ -1,9 +1,26 @@
-"""
-from SaitamaRobot.events import register
-from SaitamaRobot import OWNER_ID
-from SaitamaRobot import telethn as tbot
-import os 
+from EmiliaAnimeBot.events import register
+from EmiliaAnimeBot import OWNER_ID
+from EmiliaAnimeBot import telethn as tbot
+import os
+import random
 from PIL import Image, ImageDraw, ImageFont
+
+
+logopics = [
+ "./EmiliaAnimeBot/resources/logopics/LogoFile1.jpg",
+ "./EmiliaAnimeBot/resources/logopics/LogoFile2.jpg",
+ "./EmiliaAnimeBot/resources/logopics/LogoFile3.jpg",
+ "./EmiliaAnimeBot/resources/logopics/LogoFile4.jpg"
+]
+
+logofonts = [
+ "./EmiliaAnimeBot/resources/logofonts/RemachineScriptPersonalUseOnly-yZL3.ttf",
+ "./EmiliaAnimeBot/resources/logofonts/beyond-wonderland.regular.ttf",
+ "./EmiliaAnimeBot/resources/logofonts/Respective-VP6y.ttf"
+]
+
+pic_choice = random.choice(logopics)
+font_choice = random.choice(logofonts)
 
 
 @register(pattern="^/logo ?(.*)")
@@ -21,13 +38,13 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./EmiliaAnimeBot/resources/logobg.jpg')
+    img = Image.open(pic_choice)
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "gold"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./EmiliaAnimeBot/resources/Chopsic.otf", 330)
+    font = ImageFont.truetype(font_choice , 330)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
@@ -37,55 +54,23 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "Logo.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @EmiliaAnimeBot")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @EmiliaAnimeRobBot")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
    await event.reply(f'Error Report @TangentChats, {e}')
 
-
-
-
-@register(pattern="^/wlogo ?(.*)")
-async def lego(event):
- quew = event.pattern_match.group(1)
- if event.sender_id == OWNER_ID:
-     pass
- else:
-
-    if not quew:
-       await event.reply('Provide Some Text To Draw!')
-       return
-    else:
-       pass
- await event.reply('Creating your logo...wait!')
- try:
-    text = event.pattern_match.group(1)
-    img = Image.open('./EmiliaAnimeBot/resources/logo2.jpg')
-    draw = ImageDraw.Draw(img)
-    image_widthz, image_heightz = img.size
-    pointsize = 500
-    fillcolor = "white"
-    shadowcolor = "blue"
-    font = ImageFont.truetype("./EmiliaAnimeBot/resources/Maghrib.ttf", 1000)
-    w, h = draw.textsize(text, font=font)
-    h += int(h*0.21)
-    image_width, image_height = img.size
-    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
-    x = (image_widthz-w)/2
-    y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
-    fname2 = "Logo.png"
-    img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @EmiliaAnimeBot")
-    if os.path.exists(fname2):
-            os.remove(fname2)
- except Exception as e:
-   await event.reply(f'Error Report @TangentChats, {e}')
-
+   
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
 file_helpo = file_help.replace("_", " ")
 
 
+__mod_name__ = "Logos"
+
+__help__ = """
+
+`/logo`*:* Create a logo with text!
+
 """
+
