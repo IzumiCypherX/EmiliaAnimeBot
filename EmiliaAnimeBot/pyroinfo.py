1e4aa1427a014fc3d4ed4f02284d353c37447b88
IzumiCypherX <legitsoul45@gmail.com>
2021-09-10 20:29:10 +0530

    Create pyroinfo.py

---
+++ EmiliaAnimeBot/pyroinfo.py
@@ -0,0 +1,10 @@
+from pyrogram import Client
+from EmiliaAnimeBot import pgram
+
+x = pgram.get_me()
+
+BOT_ID = x.id
+BOT_NAME = x.first_name + (x.last_name or "")
+BOT_USERNAME = x.username
+BOT_MENTION = x.mention
+BOT_DC_ID = x.dc_id
