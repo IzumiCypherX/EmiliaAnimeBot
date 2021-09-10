ec66caf4e4560495b48822fd4aece60510dee69c
Cypher Izumi <85168398+IzumiCypherX@users.noreply.github.com>
2021-09-10 20:34:43 +0530

    Update __init__.py

--- EmiliaAnimeBot/__init__.py
+++ EmiliaAnimeBot/__init__.py
@@ -190,13 +190,6 @@ WOLVES = list(WOLVES)
 DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
-x = pgram.get_me()
-
-BOT_ID = x.id
-BOT_NAME = x.first_name + (x.last_name or "")
-BOT_USERNAME = x.username
-BOT_MENTION = x.mention
-BOT_DC_ID = x.dc_id
 
 # Load at end to ensure all prev variables have been set
 from EmiliaAnimeBot.modules.helper_funcs.handlers import (CustomCommandHandler,
