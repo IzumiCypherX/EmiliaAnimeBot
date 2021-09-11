6c88398c9eda4dc613acf0ed36dfcc3432499e7f
Cypher Izumi <85168398+IzumiCypherX@users.noreply.github.com>
2021-09-11 22:00:09 +0530

    Update paste.py

--- EmiliaAnimeBot/modules/paste.py
+++ EmiliaAnimeBot/modules/paste.py
@@ -1,12 +1,8 @@
-"""
 import requests
 from EmiliaAnimeBot import dispatcher
 from EmiliaAnimeBot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
-
-
-PASTED_IMG = "https://telegra.ph/file/94ab47d1fb4c2c3dc9109.jpg"
 
 
 @run_async
@@ -15,31 +11,29 @@ def paste(update: Update, context: CallbackContext):
     message = update.effective_message
 
     if message.reply_to_message:
-        txt = message.reply_to_message.text
+        data = message.reply_to_message.text
 
     elif len(args) >= 1:
-        txt = message.text.split(None, 1)[1]
+        data = message.text.split(None, 1)[1]
 
     else:
         message.reply_text("What am I supposed to do with this?")
         return
 
-    npaste = requests.post(post, data={"content": txt})
-   
-    url = f'www.nekobin.com/{npaste.json()['result']['key']}'
+    key = (
+        requests.post("https://nekobin.com/api/documents", json={"content": data})
+        .json()
+        .get("result")
+        .get("key")
+    )
 
-    reply_text = f'Pasted to NekoBin!'
+    url = f"https://nekobin.com/{key}"
 
+    reply_text = f"Nekofied to *Nekobin* : {url}"
 
-    message.reply_photo(
-        PASTED_IMG, caption=reply_text,
-        parse_mode=ParseMode.MARKDOWN,
-        reply_markup = InlineKeyboardMarkup(
-            [
-                InlineKeyboardButton(text="Link", url={url})
-            ]
-        )
-        )
+    message.reply_text(
+        reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
+    )
 
 
 PASTE_HANDLER = DisableAbleCommandHandler("paste", paste)
@@ -48,5 +42,5 @@ dispatcher.add_handler(PASTE_HANDLER)
 __command_list__ = ["paste"]
 __handlers__ = [PASTE_HANDLER]
 
-"""
+
 
