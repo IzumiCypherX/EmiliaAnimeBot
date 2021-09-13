fde6a58f66b0bc21b28b11f8a157920e82e11d2d
Cypher Izumi <85168398+IzumiCypherX@users.noreply.github.com>
2021-09-13 21:07:57 +0530

    Update userbottest.py

--- EmiliaAnimeBot/modules/userbottest.py
+++ EmiliaAnimeBot/modules/userbottest.py
@@ -1,5 +1,6 @@
 from EmiliaAnimeBot import emiliaub, DEV_USERS, USERBOT_PREFIX
 from pyrogram import filters
+from pyrogram.types import Message
 
 
 @emiliaub.on_message(filters.command("alive", prefixes = USERBOT_PREFIX) & filters.user(DEV_USERS))
