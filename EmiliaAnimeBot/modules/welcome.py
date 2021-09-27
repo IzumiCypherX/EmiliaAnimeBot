247ca4aba08b53404f1f080cbf7d61b9eebc528f
Cypher Izumi <85168398+IzumiCypherX@users.noreply.github.com>
2021-09-27 21:59:59 +0530

    smh

--- EmiliaAnimeBot/modules/welcome.py
+++ EmiliaAnimeBot/modules/welcome.py
@@ -4,21 +4,21 @@ import re
 import time
 from functools import partial
 
-import SaitamaRobot.modules.sql.welcome_sql as sql
-from SaitamaRobot import (DEV_USERS, LOGGER, OWNER_ID, DRAGONS, DEMONS, TIGERS,
+import EmiliaAnimeBot.modules.sql.welcome_sql as sql
+from EmiliaAnimeBot import (DEV_USERS, LOGGER, OWNER_ID, DRAGONS, DEMONS, TIGERS,
                           WOLVES, sw, dispatcher, JOIN_LOGGER)
-from SaitamaRobot.modules.helper_funcs.chat_status import (
+from EmiliaAnimeBot.modules.helper_funcs.chat_status import (
     is_user_ban_protected,
     user_admin,
 )
-from SaitamaRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from SaitamaRobot.modules.helper_funcs.msg_types import get_welcome_type
-from SaitamaRobot.modules.helper_funcs.string_handling import (
+from EmiliaAnimeBot.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from EmiliaAnimeBot.modules.helper_funcs.msg_types import get_welcome_type
+from EmiliaAnimeBot.modules.helper_funcs.string_handling import (
     escape_invalid_curly_brackets,
     markdown_parser,
 )
-from SaitamaRobot.modules.log_channel import loggable
-from SaitamaRobot.modules.sql.global_bans_sql import is_user_gbanned
+from EmiliaAnimeBot.modules.log_channel import loggable
+from EmiliaAnimeBot.modules.sql.global_bans_sql import is_user_gbanned
 from telegram import (
     ChatPermissions,
     InlineKeyboardButton,
