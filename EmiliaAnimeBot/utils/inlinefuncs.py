7aea932cf8bf76bb16b4a75419ca006d84845d61
Cypher Izumi <85168398+IzumiCypherX@users.noreply.github.com>
2021-09-28 22:04:14 +0530

    Update inlinefuncs.py

--- EmiliaAnimeBot/utils/inlinefuncs.py
+++ EmiliaAnimeBot/utils/inlinefuncs.py
@@ -34,7 +34,7 @@ from EmiliaAnimeBot.utils.keyboard import ikb
 from EmiliaAnimeBot.core.tasks import _get_tasks_text, all_tasks, rm_task
 from EmiliaAnimeBot.core.types import InlineQueryResultCachedDocument
 from EmiliaAnimeBot.modules.info import get_chat_info, get_user_info
-from EmiliaAnimeBot.modules.music import download_youtube_audio
+from EmiliaAnimeBot.modules.media import download_youtube_audio
 from EmiliaAnimeBot.utils.functions import test_speedtest
 from EmiliaAnimeBot.utils.pastebin import paste
 
