90f63d1abb4dddb916bd7d31231d05e4e93fe2d9
Cypher Izumi <85168398+IzumiCypherX@users.noreply.github.com>
2021-09-30 08:06:46 +0530

    v1.0 Update

--- EmiliaAnimeBot/__main__.py
+++ EmiliaAnimeBot/__main__.py
@@ -616,8 +616,6 @@ def main():
     dispatcher.add_handler(help_callback_handler)
     dispatcher.add_handler(settings_callback_handler)
     dispatcher.add_handler(migrate_handler)
-    dispatcher.add_handler(donate_handler)
-
     dispatcher.add_error_handler(error_callback)
 
     if WEBHOOK:
