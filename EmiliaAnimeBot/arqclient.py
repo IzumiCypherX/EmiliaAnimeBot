b422643e4be25eb6c93f4eca9f05da34c57a4749
Cypher Izumi <85168398+IzumiCypherX@users.noreply.github.com>
2021-11-29 22:51:15 +0530

    Update arqclient.py

--- EmiliaAnimeBot/arqclient.py
+++ EmiliaAnimeBot/arqclient.py
@@ -2,7 +2,7 @@ from aiohttp import ClientSession
 from Python_ARQ import ARQ
 from EmiliaAnimeBot import ARQ_API_KEY
 
-ARQ_API_URL = "https://thearq.tech"
+ARQ_API_URL = "https://grambuilders.tech"
 
 aiohttpsession = ClientSession()
 
